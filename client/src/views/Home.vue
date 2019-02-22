7eca6425522222cbd7c330f77e62eb5d4a0b73f6
Ejiro Edwin Obamrevwo <EjiroEdwin@gmail.com>
2019-02-22 01:04:58 +0100

    Initial Commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Community!</h1>
+      <p class="lead">A Fully Open source forum made with love by Ejiro Edwin.</p>
+      <hr class="my-4">
+      <p>This forum is for the Community, Be Kind; Be Helpful</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">SignUp</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
